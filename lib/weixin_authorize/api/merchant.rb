45fbbe9cd69cf1797abd124d02905c71727f6cb7
jack_nie <hust.ud.hui@gmail.com>
2016-01-21 14:39:58 +0800

    add api params to http post

--- lib/weixin_authorize/api/merchant.rb
+++ lib/weixin_authorize/api/merchant.rb
@@ -6,19 +6,13 @@ module WeixinAuthorize
       # 根据订单状态/创建时间获取订单详情
       # https://api.weixin.qq.com/merchant/order/getbyfilter?access_token=ACCESS_TOKEN
       def get_by_filter(status="nil", begintime="nil", endtime="nil")
-        get_by_filter_url = "#{merchant_base_url}/getbyfilter"
+        get_by_filter_url = "/merchant/order/getbyfilter"
         post_body = {
           status: status,
           begintime: begintime,
           endtime: endtime
         }
-        http_post(get_by_filter_url, post_body) 
-      end
-    
-      private
-
-      def merchant_base_url
-        "/merchant"
+        http_post(get_by_filter_url, post_body, {  }, "api")
       end
     end
   end
